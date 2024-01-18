300312cd4f10a0dea762095be1494c25d4aa901d
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2024-01-17 22:29:53 -0600

    Spacers created

--- app/src/main/java/com/leksilab/descuentosapp/components/BodyComponents.kt
+++ app/src/main/java/com/leksilab/descuentosapp/components/BodyComponents.kt
@@ -1,2 +1,19 @@
 package com.leksilab.descuentosapp.components
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SpaceH(size: Dp = 5.dp) {
+    Spacer(modifier = Modifier.height(size))
+}
+
+@Composable
+fun SpaceW(size: Dp = 5.dp) {
+    Spacer(modifier = Modifier.width(size))
+}
--- app/src/main/java/com/leksilab/descuentosapp/views/HomeView.kt
+++ app/src/main/java/com/leksilab/descuentosapp/views/HomeView.kt
@@ -1,8 +1,37 @@
 package com.leksilab.descuentosapp.views
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeView() {
+    Scaffold(topBar = {
+        TopAppBar(
+            title = { Text(text = "Porcentage Descuentos") },
+            backgroundColor = MaterialTheme.colors.primary
+        )
+    }) {
+        HomeViewContent(it)
+    }
 
+}
+
+@Composable
+fun HomeViewContent(paddingValues: PaddingValues) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(paddingValues = paddingValues)
+        .padding(10.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "HOLIS PERRIS")
+    }
 }
