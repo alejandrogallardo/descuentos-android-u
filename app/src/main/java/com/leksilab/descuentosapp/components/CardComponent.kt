a828cef535ff497dc020a517671b8686c01e9324
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2024-01-17 22:02:11 -0600

    Inicio del proyecto

---
+++ app/src/main/java/com/leksilab/descuentosapp/components/CardComponent.kt
@@ -0,0 +1,2 @@
+package com.leksilab.descuentosapp.components
+
---
+++ app/src/main/java/com/leksilab/descuentosapp/views/HomeView.kt
@@ -0,0 +1,8 @@
+package com.leksilab.descuentosapp.views
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeView() {
+
+}
