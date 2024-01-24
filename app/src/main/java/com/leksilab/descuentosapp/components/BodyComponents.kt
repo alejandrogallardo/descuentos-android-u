93a0f5d86290f9201a9b5c59812e07d782132d9e
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2024-01-24 09:48:34 -0600

    fin del proyecto

--- app/src/main/java/com/leksilab/descuentosapp/components/BodyComponents.kt
+++ app/src/main/java/com/leksilab/descuentosapp/components/BodyComponents.kt
@@ -1,10 +1,12 @@
 package com.leksilab.descuentosapp.components
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
@@ -16,4 +18,50 @@ fun SpaceH(size: Dp = 5.dp) {
 @Composable
 fun SpaceW(size: Dp = 5.dp) {
     Spacer(modifier = Modifier.width(size))
+}
+
+@Composable
+fun MainTextField(value: String, onValueChange: (String) -> Unit, label: String) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(text = label) },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 30.dp)
+    )
+}
+
+@Composable
+fun MainButton(text: String, color: Color = MaterialTheme.colors.primary, onClick: () -> Unit)  {
+    OutlinedButton(onClick = onClick, colors = ButtonDefaults.outlinedButtonColors(
+        contentColor = color,
+        backgroundColor = Color.Transparent
+    ),
+    modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 30.dp)) {
+        Text(text = text)
+    }
+}
+
+@Composable
+fun Alert(
+    title: String,
+    message: String,
+    confirmText: String,
+    onConfirmClick: () -> Unit,
+    onDismissClick: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismissClick,
+        title = { Text(text = title) },
+        text = { Text(text = message) },
+        confirmButton = {
+            Button(onClick = { onConfirmClick() }) {
+                Text(text = confirmText)
+            }
+        }
+    )
 }
--- app/src/main/java/com/leksilab/descuentosapp/views/HomeView.kt
+++ app/src/main/java/com/leksilab/descuentosapp/views/HomeView.kt
@@ -5,16 +5,18 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.leksilab.descuentosapp.components.*
 
 @Composable
 fun HomeView() {
     Scaffold(topBar = {
         TopAppBar(
-            title = { Text(text = "Porcentage Descuentos") },
+            title = { Text(text = "Calcular Descuentos") },
             backgroundColor = MaterialTheme.colors.primary
         )
     }) {
@@ -32,6 +34,49 @@ fun HomeViewContent(paddingValues: PaddingValues) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "HOLIS PERRIS")
+        var precio by remember { mutableStateOf("") }
+        var descuento by remember { mutableStateOf("") }
+        var precioDescuento by remember { mutableStateOf(0.0) }
+        var totalDescuento by remember { mutableStateOf(0.0) }
+        var showAlert by remember { mutableStateOf(false) }
+        TwoCards(title1 = "Total:", number1 = totalDescuento, title2 = "Descuento:", number2 = precioDescuento)
+        MainTextField(value = precio, onValueChange = { precio = it }, label = "Precio")
+        SpaceH()
+        MainTextField(value = descuento, onValueChange = { descuento = it }, label = "Descuento")
+        SpaceH(10.dp)
+        MainButton(text = "Generar Descuento") {
+            if (precio != "" && descuento != "") {
+                precioDescuento = calcularPrecio(precio.toDouble(), descuento.toDouble())
+                totalDescuento = calcularDescuento(precio.toDouble(), descuento.toDouble())
+            } else {
+                showAlert = true
+            }
+        }
+        SpaceH()
+        MainButton(text = "Limpiar", color = Color.Red) {
+            precio = ""
+            descuento = ""
+            precioDescuento = 0.0
+            totalDescuento = 0.0
+        }
+
+        if (showAlert) {
+            Alert(
+                title = "Alerta",
+                message = "Los campos no puden ir vacios",
+                confirmText = "Aceptar",
+                onConfirmClick = { showAlert = false }) { }
+        }
+
     }
+}
+
+fun calcularPrecio(precio: Double, descuento: Double): Double {
+    val res = precio - calcularDescuento(precio, descuento)
+    return kotlin.math.round(res * 100) / 100.0
+}
+
+fun calcularDescuento(precio: Double, descuento: Double): Double {
+    val res = precio * (1 - descuento / 100)
+    return kotlin.math.round(res * 100) / 100.0
 }
